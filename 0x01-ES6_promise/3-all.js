e4bebfa3ed5377923b922cb2a587803805e7ada7
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-12-23 01:14:49 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,11 @@
-export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({ status: 200, body: "success" }))
-    .catch(() => new Error())
-    .finally(() => console.log("Got a response from the API"));
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((response) => {
+      console.log(
+        `${response[0].body} ${response[1].firstName} ${response[1].lastName}`
+      );
+    })
+    .catch(() => console.log("Signup system offline"));
 }
