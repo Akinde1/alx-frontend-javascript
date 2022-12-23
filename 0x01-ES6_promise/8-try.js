6a95a56480ded8e3c042cf52e8397fbe82f6120c
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-12-23 01:08:49 +0100

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,5 +1,7 @@
-export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    resolve({ firstName, lastName });
-  });
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw "cannot divide by 0";
+  } else {
+    return numerator / denominator;
+  }
 }
