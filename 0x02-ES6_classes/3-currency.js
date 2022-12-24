27d69efb524e68322b25b1de9936bc71e1865915
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-12-24 02:46:04 +0100

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,42 +1,38 @@
 export default class Currency {
   constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
     this._code = code;
     this._name = name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(newCode) {
-    if (!typeof newCode === "string") {
-      throw new TypeError("Code must be a string");
-    }
-    this._code = newCode;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(newName) {
-    if (!typeof newName === "string") {
-      throw new TypeError("Name must be a string");
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    this._name = newName;
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
   }
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
-  }
-
-  static convertPreice(amount, conversionRate) {
-    if (!typeof amount === "number") {
-      throw new TypeError("Amount must be a number");
-    }
-    if (!typeof conversionRate === "number") {
-      throw new TypeError("Conversion rate must be a number");
-    }
-    return amount * conversionRate;
   }
 }
