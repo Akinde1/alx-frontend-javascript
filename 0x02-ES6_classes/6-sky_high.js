c55f245078d46c174462a338f0eaac16547b3546
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-12-24 02:49:47 +0100

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,18 +1,18 @@
-import Building from "./5-building";
+import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
-  // floors getter
   get floors() {
-    return this._floors;
+    return (this._floors);
   }
 
-  // evacuationWarningMessage method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return (`Evacuate slowly the ${this.floors} floors`);
   }
 }
+
+export default SkyHighBuilding;
